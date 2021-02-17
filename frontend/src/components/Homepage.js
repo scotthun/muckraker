99befca10ffd9ac531542f3897b1afce73730635
scotthun <scotthung1@gmail.com>
2021-02-16 16:12:43 -0800

    Refactor Homepage from class component to functional

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -5,28 +5,19 @@ import Introduction from './Introduction.js'
 import Spacer from './Spacer.js'
 import LookupLegislators from './LookupLegislators.js'
 
-export default class Homepage extends React.Component {
-  constructor(props) {
-      super(props);
-      
-      
-  }
+export default function Homepage() {
 
-  render() {
-      return (
-          <Layout>
-            <Spacer/>
-            <Introduction />
-            <Spacer/>
-            <div id="dashboard"></div>
-            <Spacer/>
-            <Dashboard />
-            <div id='legislators'></div>
-            <Spacer />
-            <LookupLegislators />
-          </Layout>
-      );
-  }
-
-
+  return (
+    <Layout>
+      <Spacer/>
+      <Introduction />
+      <Spacer/>
+      <div id="dashboard"></div>
+      <Spacer/>
+      <Dashboard />
+      <div id='legislators'></div>
+      <Spacer />
+      <LookupLegislators />
+    </Layout>
+  );
 }
